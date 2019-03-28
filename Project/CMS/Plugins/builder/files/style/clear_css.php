ee7342db6146d672ed3f474d96b135fd24719a53
unknown <quartzcms@gmail.com>
2019-03-28 19:40:30 -0400

    removal of builder

--- Project/CMS/Plugins/builder/files/style/clear_css.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = isset($_POST['file']) ? $_POST['file'] : '';
-
-$handle = fopen('../project/style/edits/'.$file, 'w');
-fwrite($handle, '/**/');
-fclose($handle);
-
-echo json_encode(array('success'=> true));
-?>
