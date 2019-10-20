396ff4c409c4223cde3a027e55fe3ee78622f46d
Alexandre Lefebvre <quartzcms@gmail.com>
2019-10-20 19:36:50 -0400

    new update manager

--- Project/CMS/Languages/Admin/En/Config.php
+++ Project/CMS/Languages/Admin/En/Config.php
@@ -48,5 +48,24 @@ define('CONFIGURATION_MAIL_SMTP_SECURE_SSL', 'SSL');
 define('CONFIGURATION_MAIL_SMTP_SECURE_TLS', 'TLS');
 define('CONFIGURATION_MAIL_SMTP_PORT', 'SMTP Port');
 
+define('CONFIGURATION_UPDATE', 'CMS update');
+define('UPDATE_INTRO', 'You can update the CMS below to the latest. Please make sure you you have a backup of the CMS files before proceding.');
+define('UPDATE_GIT_INSTALLED', 'GIT command line is installed. You can proceed to the installation.');
+define('UPDATE_CMS', 'Click on the update button below to update the CMS');
+define('UPDATE_COMPOSER_WILL_BE_UPDATED', 'The composer.phar file will be updated and all the vendor repository as well. Please make sure you have the PHP client command line');
+define('UPDATE_GIT_NOT_INSTALLED', 'GIT command line is not installed. You will need to proceed manually.');
+define('UPDATE_FILE_NEEDS_BE_COPIED', 'Files will need to be copied. More information on manual (Version 3.x to latest) <a href="https://www.quartzcms.ca/list/download/tel3/0/Version_3_x" target="_blank">here</a>');
+define('UPDATE_COMPOSER_NEED_BE_UPDATED', 'The composer.phar will need to be updated manually. More information on manual update (Version 3.x to latest) <a href="https://www.quartzcms.ca/list/download/tel3/0/Version_3_x" target="_blank">here</a>');
+define('UPDATE_CMS_IS_UPTODATE', 'The CMS already have the latest version installed. If you have errors you can reupload manually the files by following this (Version 3.x to latest) <a href="https://www.quartzcms.ca/list/download/tel3/0/Version_3_x" target="_blank">documentation</a>');
+define('TEMPLATES_OVERRIDE_CHECKUP', 'Template checkup');
+define('TEMPLATES_OVERRIDE_NOT_MATCHED', 'does not match');
+define('TEMPLATES_OVERRIDE_MATCHED', 'does match');
+define('TEMPLATES_OVERRIDE_ALL_MATCHED', 'All the templates matches the override.');
+define('TEMPLATES_OVERRIDE_NOT_MATCHED_ALL', 'Some of the templates does not match the override of the CMS. Please review the new changes and make sure they are functional.');
+define('UPDATE_FUNCTION_EXEC_DONT_EXIST', 'The PHP function exec() don\'t exist or is not accessible.');
+define('UPDATE_COMPOSER_NEW_FILES', 'Attention ! Some Composer repositories will be removed after the update. You will have to reinstall them. If you do not want this to happen, please update the CMS manually and keep your modifications in the composer.json file.');
+define('UPDATE_COMPOSER_NEW_FILES_DONT_EXIST', 'The following repository is new:');
+
+
 
 ?>
